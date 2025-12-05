cca31f50ef31693e06c0632b5ec16192aecddefa
venkata krishna <kakarlavenkatakrishna9@gmail.com>
2025-12-05 17:58:53 +0530

    add: implement theme management with encryption for localStorage

--- Ai-Resume-Builder/src/features/themeSlice.js
+++ Ai-Resume-Builder/src/features/themeSlice.js
@@ -1,15 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { encryptData, decryptData } from "../utils/Encryption";
 
 
+// Key we want to use in localStorage (will be encrypted)
+const STORAGE_KEY = "darkMode";
+
+function  encrypttheme(){
+  // Encrypt the key
+  const encryptedKey = encryptData(STORAGE_KEY);
+  
+  // Get value from localStorage
+  const encryptValue = localStorage.getItem(encryptedKey);
+
+  // if nothing  is saved yet  default false
+  if(!encryptValue) return false;
+
+  // Get value from localStorage
+  const decryptValue = decryptData(encryptValue)
+
+  // Convert string to boolean
+  return decryptValue === "true";
+}
+// Initial state
+const initialTheme = encrypttheme();
+
+// Create slice
 const themeSlice = createSlice({
-    name :"theme",
-    initialState : false,
-    reducers :{
-        toggleTheme(state){
-            return !state
-        }
-    }
-})
+  name: "theme",
+  initialState: initialTheme,
+  reducers: {
+    toggleTheme(state) {
+      const newTheme = !state;
 
-export const {toggleTheme} = themeSlice.actions
-export default themeSlice.reducer
+      // Encrypt key and value
+      const encryptedKey = encryptData(STORAGE_KEY);
+      const encryptedValue = encryptData(newTheme.toString());
+
+      // Save encrypted data to localStorage
+      localStorage.setItem(encryptedKey, encryptedValue);
+
+
+      return newTheme;
+    }
+  }
+});
+
+export const { toggleTheme } = themeSlice.actions;
+export default themeSlice.reducer;
